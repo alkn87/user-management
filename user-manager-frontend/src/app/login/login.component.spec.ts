09ff880dbf5429bdc2db817adc785d77f870d4b8
Sasan Jaghori <jaghori.sasan@gmail.com>
2022-01-06 21:25:36 +0100

    * fixing warnings

--- user-manager-frontend/src/app/login/login.component.spec.ts
+++ user-manager-frontend/src/app/login/login.component.spec.ts
@@ -5,14 +5,33 @@ import {FormBuilder} from "@angular/forms";
 import {AuthService} from "../core/auth/auth.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
+    const materialDesignModules = [
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatIconModule,
+      MatMenuModule,
+      MatToolbarModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule
+    ]
+
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [materialDesignModules, HttpClientTestingModule, RouterTestingModule],
       providers: [FormBuilder, AuthService],
       declarations: [ LoginComponent ]
     })
--- user-manager-frontend/src/app/register/register.component.spec.ts
+++ user-manager-frontend/src/app/register/register.component.spec.ts
@@ -5,14 +5,34 @@ import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
 import {FormBuilder} from "@angular/forms";
 import {AuthService} from "../core/auth/auth.service";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
+
+    const materialDesignModules = [
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatIconModule,
+      MatMenuModule,
+      MatToolbarModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule
+    ]
+
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule, materialDesignModules],
       providers: [FormBuilder, AuthService],
       declarations: [ RegisterComponent ]
     })
