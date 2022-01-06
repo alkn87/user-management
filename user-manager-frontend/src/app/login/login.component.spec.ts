b9a4f3076c3a949a17b9efafed683d6b35d32d04
Sasan Jaghori <jaghori.sasan@gmail.com>
2022-01-06 21:57:27 +0100

    * added tests for login component

--- user-manager-frontend/src/app/login/login.component.spec.ts
+++ user-manager-frontend/src/app/login/login.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
 import {FormBuilder} from "@angular/forms";
 import {AuthService} from "../core/auth/auth.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
@@ -13,6 +13,8 @@ import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -33,9 +35,9 @@ describe('LoginComponent', () => {
     await TestBed.configureTestingModule({
       imports: [materialDesignModules, HttpClientTestingModule, RouterTestingModule],
       providers: [FormBuilder, AuthService],
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -47,4 +49,26 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set instance correctly', () => {
+    expect(component).not.toBeNull();
+  });
+
+  it('should call auth login method', fakeAsync(() => {
+    let loginElement: DebugElement;
+    const debugElement = fixture.debugElement;
+    const authService = debugElement.injector.get(AuthService);
+    const loginSpy = spyOn(authService, 'loginUser').and.callThrough();
+    const testUser = {
+      username: 'testuser123',
+      password: '12345'
+    };
+    component.loginForm.controls['username'].setValue(testUser.username);
+    component.loginForm.controls['password'].setValue(testUser.password);
+    loginElement = fixture.debugElement.query(By.css('form'));
+    loginElement.triggerEventHandler('ngSubmit', null);
+    expect(loginSpy).toHaveBeenCalledTimes(1);
+    // check that service is called once
+    // ^ this fails without correct login!
+  }));
 });
